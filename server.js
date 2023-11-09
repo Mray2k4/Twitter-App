ffd14b326616367bc80d4aadf30fbe755a275746
Marque Ray <mray2k4@yahoo.com>
2023-11-08 21:11:13 -0800

    Created internal api

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/static/index.html'));
+})
+
+app.get('/api/character', function(req, res){
+   // res.send(character);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
