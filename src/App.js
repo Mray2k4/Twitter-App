46ce07821cedd2250090f744825bbc58318d22e9
Marque Ray <mray2k4@yahoo.com>
2023-11-14 13:51:02 -0800

    added navbar

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import NavScrollExample from "./navbar.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavScrollExample/>
+        <p>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
