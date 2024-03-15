af1c7086f2bacffdc3926b78137087e72825ffda
Marque Ray <mray2k4@yahoo.com>
2024-03-14 21:58:50 -0700

    Added backend folder and put server.js file in it

--- src/App.js
+++ src/App.js
@@ -59,6 +59,32 @@ async function apiList() {
 //     apiList();
 //   }, [])
 
+// New Code //
+
+// const [title, setTitle] = useState([])
+// const url = 'https://jsonplaceholder.typicode.com/posts'
+// const token = 'AAAAAAAAAAAAAAAAAAAAAPly9QAAAAAAQP4Qf6PfN0NeU4L5keo%2B7kae%2Fs0%3DEQIp2W7jkVldFBLvOOFtSJXl2vWEe3f1J1STKMTyWEbsogNYfE'
+
+// useEffect(() => {
+//   const fetchData = async () => {
+//     const res = await fetch('https://api.twitter.com/2/tweets/1742229265599344708')
+//     const jsonResults = await res.json()
+//     const result = await axios.get(url, {
+//           headers: { 
+//             Authorization: `Bearer ${token}`,
+//             'Access-Control-Allow-Headers' : "Content-Type",
+//             "Access-Control-Allow-Origin": "",
+//             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+//           }
+//         })
+//     setTitle(jsonResults)
+//     console.log(result.data.title)
+//   };
+//     fetchData();
+//   }, [])
+
+
+
 
   
 return (
